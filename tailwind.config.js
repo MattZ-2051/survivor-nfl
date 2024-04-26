49e52e2f22957952d5583196467166bbb1571475
Matt Zamora <mzamora2899@gmail.com>
2024-04-26 16:39:38 -0700

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
+	theme: {
+		extend: {}
+	},
+	plugins: [addVariablesForColors]
+};
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
